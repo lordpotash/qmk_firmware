1c42a974f376b3258c1b4edf774060a56b7912ec
lordpotash <punarvasu.sangam@gmail.com>
2025-05-06 00:24:47 -0700

    q3_max_TD_MPLY_MNXT

--- keyboards/keychron/q3_max/ansi_encoder/keymaps/lordpotash/keymap.c
+++ keyboards/keychron/q3_max/ansi_encoder/keymaps/lordpotash/keymap.c
@@ -24,6 +24,10 @@ enum layers {
     WIN_FN,
 };
 
+enum {
+    TD_PLY_NXT,
+};
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_tkl_ansi(
@@ -43,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______),
 
     [WIN_BASE] = LAYOUT_tkl_ansi(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MPLY,    KC_PSCR,  C(S(A(KC_M))), RGB_MOD,
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     TD_PLY_NXT,    KC_PSCR,  C(S(A(KC_M))), RGB_MOD,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,
@@ -57,6 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,
         _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,              _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______),
+
 };
 
 // clang-format on
@@ -79,6 +84,11 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     }
     return false;
 }
+
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Media Play, twice for Media Skip
+    [TD_PLY_NXT] = ACTION_TAP_DANCE_DOUBLE(KC_MPLY, KC_MNXT),
+};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (!process_record_keychron_common(keycode, record)) {
--- keyboards/keychron/q3_max/ansi_encoder/rules.mk
+++ keyboards/keychron/q3_max/ansi_encoder/rules.mk
@@ -1,1 +1,1 @@
-# This file intentionally left blank
+TAP_DANCE_ENABLE = yes
