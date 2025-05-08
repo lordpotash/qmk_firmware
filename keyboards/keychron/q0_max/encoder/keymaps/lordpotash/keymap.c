450f2cd9243a86c7bc2fc081f5c8072141f16446
lordpotash <punarvasu.sangam@gmail.com>
2025-05-07 17:02:32 -0700

    q0max_mod-tap_PENT/LALT

--- keyboards/keychron/q0_max/encoder/keymaps/lordpotash/keymap.c
+++ keyboards/keychron/q0_max/encoder/keymaps/lordpotash/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,	 KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
         MC_2,	 KC_P7,	  KC_P8,   KC_P9,   KC_PPLS,
         MC_3,	 KC_P4,   KC_P5,   KC_P6,
-        MO(L2),	 KC_P1,	  KC_P2,   KC_P3,   KC_PENT,
+        MO(L2),	 KC_P1,	  KC_P2,   KC_P3,   LALT_T(KC_PENT),
         MO(FN),  KC_P0,            KC_PDOT         ),
 
     [FN] = LAYOUT_tenkey_27(
